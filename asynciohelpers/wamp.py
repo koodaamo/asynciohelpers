688995c80ca9f0116adb590cb9f9735ee3f02c83
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-11-15 15:53:03 +0200

    have WAMP mixin provide _setup, assign loop to session component, remove __new__

--- asynciohelpers/wamp.py
+++ asynciohelpers/wamp.py
@@ -1,25 +1,27 @@
+import asyncio
 from autobahn.wamp.types import ComponentConfig
 from autobahn.asyncio.websocket import WampWebSocketClientFactory
 from autobahn.websocket.util import parse_url
 
+from .util import loggerprovider
 
+
+@loggerprovider
 class WAMPServiceMixin:
    "base mixin that provides WAMP configuration plus transport and component factory"
 
-   wmp_url = None
-   wmp_realm = None
-   wmp_sessioncomponent = lambda x: None
-   wmp_serializers = None
-   wmp_ssl = None #  set to True or False for forced enable/disable
-   wmp_extra = None # {"winservice": False} # by default not running as a Windows service
+   async def _connect(self):
+      "wait all the way until component has joined"
+      await super()._connect()
+      self._protocol._session_joined = asyncio.Future(loop=self._loop)
 
+   async def _setup(self):
+      await super()._setup()
+      await self._protocol._session_joined
 
    @property
    def _transport_factory(self):
       "create and return the transport factory"
-
-      assert hasattr(self, "wmp_url")
-      assert hasattr(self, "wmp_serializers")
 
       try:
          factory = WampWebSocketClientFactory(self._component, url=self.wmp_url, serializers=self.wmp_serializers, loop=self._loop)
@@ -30,9 +32,7 @@ class WAMPServiceMixin:
 
 
    def _component(self):
-      "create and return the component factory"
-
-      assert getattr(self, "wmp_realm", None) is not None
+      "component factory method that creates and returns the component"
 
       try:
          cfg = ComponentConfig(self.wmp_realm, self.wmp_extra)
@@ -44,23 +44,6 @@ class WAMPServiceMixin:
       except Exception as exc:
          raise Exception("could not instantiate WAMP component: %s" % str(exc))
       else:
+         session._loop = self._loop
          return session
 
-
-   def __new__(cls, *args, **kwargs):
-
-      (isSecureURL, host, port, resource, path, params) = parse_url(cls.wmp_url)
-
-      cls._host = host
-      cls._port = port
-
-      if not getattr(cls, "wmp_ssl", None):
-         cls._ssl = isSecureURL
-      elif cls.wmp_ssl and not isSecureURL:
-         raise RuntimeError(
-            'the wmp_ssl class variable of %s conflicts with the "ws:" prefix '
-            'of the wmp_url variable. Did you mean to use "wss:"?' % cls.__name__)
-      else:
-         cls._ssl = cls.wmp_ssl
-
-      return object.__new__(cls)
