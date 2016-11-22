439d96b18374509a00877bd966734f6cf267c947
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-11-22 16:07:14 +0200

    have setup wait for WAMP realm joining

--- asynciohelpers/wamp.py
+++ asynciohelpers/wamp.py
@@ -3,33 +3,25 @@ from autobahn.wamp.types import ComponentConfig
 from autobahn.asyncio.websocket import WampWebSocketClientFactory
 from autobahn.websocket.util import parse_url
 
-from .util import loggerprovider
 
-
-@loggerprovider
 class WAMPServiceMixin:
    "base mixin that provides WAMP configuration plus transport and component factory"
 
-   async def _connect(self):
-      "wait all the way until component has joined"
-      await super()._connect()
-      self._protocol._session_joined = asyncio.Future(loop=self._loop)
-
    async def _setup(self):
       await super()._setup()
-      await self._protocol._session_joined
+      await self._protocol.factory._session_joined
 
    @property
    def _transport_factory(self):
       "create and return the transport factory"
-
       try:
          factory = WampWebSocketClientFactory(self._component, url=self.wmp_url, serializers=self.wmp_serializers, loop=self._loop)
       except Exception as exc:
          raise Exception("could not build transport factory: %s" % exc)
       else:
+         factory._session_joined = asyncio.Future(loop=self._loop)
+         self._logger.info("WAMP connecting to %s, realm '%s'" % (self.wmp_url, self.wmp_realm))
          return factory
-
 
    def _component(self):
       "component factory method that creates and returns the component"
