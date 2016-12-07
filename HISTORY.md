772dfa27f23585e850afc123cd57cd887f5cd41f
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-12-07 15:53:15 +0200

    bump to 0.3 rc, add HISTORY

---
+++ HISTORY.md
@@ -0,0 +1,5 @@
+0.3rc0 (2016-12-07)
+-----------------
+
+- Add default waiter and runner that are restarted
+  if they raise an Exception
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2'
+version = "0.3rc0"
 
 setup(name='asynciohelpers',
       version=version,
